bc97b37532857fee6710fb451d0d8c288a2265df
HelloJung <dudwns0929@naver.com>
2018-11-13 00:58:18 +0900

    1

---
+++ src/main/java/org/jj/mapper/ReplyMapper.java
@@ -0,0 +1,9 @@
+package org.jj.mapper;
+
+import org.jj.domain.ReplyVO;
+
+public interface ReplyMapper {
+	
+	public int insert(ReplyVO vo);
+
+}
---
+++ src/test/java/org/jj/controller/ReplyMapperTests.java
@@ -0,0 +1,46 @@
+package org.jj.controller;
+
+import java.util.stream.IntStream;
+
+import org.jj.domain.ReplyVO;
+import org.jj.mapper.ReplyMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class ReplyMapperTests {
+
+	private int[] bnoArr = {19,18,17,16,15};
+	
+	@Setter(onMethod_=@Autowired)
+	private ReplyMapper mapper;
+	
+	@Test
+	public void testMapper() {
+		log.info(mapper);
+	}
+	
+	@Test
+	public void testInsert() {
+		
+		IntStream.rangeClosed(1, 10).forEach(i -> {
+			
+			ReplyVO vo = new ReplyVO();
+			
+			vo.setBno(bnoArr[i % 5]);
+			vo.setReply("댓글테스트" + i);
+			vo.setReplyer("replyer"+i);
+			
+			mapper.insert(vo);
+		});
+		
+	}
+}
